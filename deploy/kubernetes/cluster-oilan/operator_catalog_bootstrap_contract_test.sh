748fe7211f56c5191053a664b7aaa4b868dc6621
doops <test@test.com>
2026-07-17 01:54:12 +0800

    fix(oilan): mount bootstrap credentials

--- deploy/kubernetes/cluster-oilan/operator_catalog_bootstrap_contract_test.sh
+++ deploy/kubernetes/cluster-oilan/operator_catalog_bootstrap_contract_test.sh
@@ -12,6 +12,11 @@ grep -Fq -- "- dev-org" "${JOB}"
 grep -Fq -- "- admin@agentsmesh.local" "${JOB}"
 grep -A1 -F -- "- --model-resource-id" "${JOB}" | grep -Fq -- '- "1"'
 grep -A1 -F -- "- --runtime-image-id" "${JOB}" | grep -Fq -- '- "4"'
+grep -Fq "mountPath: /app/access-token" "${JOB}"
+grep -Fq "secretName: agentsmesh-access-token" "${JOB}"
+grep -Fq "mountPath: /app/ssl" "${JOB}"
+grep -Fq "secretName: agentsmesh-pki-ca" "${JOB}"
+grep -Fq "mountPath: /data" "${JOB}"
 grep -Fq "bootstrap_operator_catalog" "${DEPLOY}"
 grep -Fq "job/operator-catalog-bootstrap" "${DEPLOY}"
 
