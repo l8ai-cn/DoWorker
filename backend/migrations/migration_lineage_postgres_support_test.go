bd35e995f97875be20b4a8c6ed323104c5bff169
doops <test@test.com>
2026-07-17 09:25:15 +0800

    test(migrations): track latest migration version

--- backend/migrations/migration_lineage_postgres_support_test.go
+++ backend/migrations/migration_lineage_postgres_support_test.go
@@ -17,7 +17,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const latestMigrationVersion = 224
+const latestMigrationVersion = 225
 
 //go:embed testdata/legacy_000209/*.sql
 var legacy000209Fixtures embed.FS
